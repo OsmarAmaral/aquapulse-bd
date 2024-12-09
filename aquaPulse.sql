4ef3dbbef0cf47dc0e3e8682b8fcc8df7cbdd2f4
OsmarAmaral <135924752+OsmarAmaral@users.noreply.github.com>
2024-12-09 07:31:01 -0400

    Update aquaPulse.sql

--- aquaPulse.sql
+++ aquaPulse.sql
@@ -1,1 +1,19 @@
+-- AquaPulse - Sistema de irrigação automática
+
+CREATE TABLE tbAdministradores (
+  id_adm SERIAL PRIMARY KEY,
+  nome_adm VARCHAR(100),
+  sobrenome_adm VARCHAR(100),
+  email_adm VARCHAR(100),
+  cargo_adm VARCHAR(100)
+);
+
+CREATE TABLE tbUsuarios (
+  id_usuario SERIAL PRIMARY KEY,
+  nome_usuario VARCHAR(100),
+  sobrenome_usuario VARCHAR(100),
+  email_usuario VARCHAR(255),
+  cidade_usuario VARCHAR(255),
+  estado_usuario VARCHAR(255)
+);
 
