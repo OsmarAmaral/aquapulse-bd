de0c85828f918a94a2efe0bb64c2b315ad1ba423
OsmarAmaral <135924752+OsmarAmaral@users.noreply.github.com>
2024-12-12 14:26:53 -0400

    Update aquaPulse.sql

--- aquaPulse.sql
+++ aquaPulse.sql
@@ -4,7 +4,7 @@ CREATE TABLE tbAdministradores (
   id_adm SERIAL PRIMARY KEY,
   nome_adm VARCHAR(100),
   sobrenome_adm VARCHAR(100),
-  email_adm VARCHAR(100),
+  email_adm VARCHAR(100) UNIQUE,
   cargo_adm VARCHAR(100)
 );
 
@@ -12,19 +12,17 @@ CREATE TABLE tbUsuarios (
   id_usuario SERIAL PRIMARY KEY,
   nome_usuario VARCHAR(100),
   sobrenome_usuario VARCHAR(100),
-  email_usuario VARCHAR(255),
+  email_usuario VARCHAR(255) UNIQUE,
   cidade_usuario VARCHAR(255),
   estado_usuario VARCHAR(255)
 );
 
 INSERT INTO tbAdministradores (nome_adm, sobrenome_adm, email_adm, cargo_adm)
   VALUES ('Osmar', 'Araujo', 'osmar@gmail.com', 'chefe'),
-         ('Iago', 'Bortoluzzi', 'iago@gmail.com', 'chefe'),
+         ('Iago', 'Bortoluzzi', 'iago1@gmail.com', 'chefe'),
          ('Pedro', 'Alves', 'pedro@gmail.com', 'estagiario'),
-         ('Iago', 'Paiva', 'iago@gmail.com', 'estagiario');
+         ('Iago', 'Paiva', 'iago2@gmail.com', 'estagiario');
          
 INSERT INTO tbUsuarios (nome_usuario, sobrenome_usuario, email_usuario, cidade_usuario, estado_usuario)
-  VALUES ('Leonardo', 'Ferreira', 'leonardo@gmail.com', 'Rondonópolis', 'MT');
-  
-  select *
-  from tbAdministradores
+  VALUES ('Ana', 'Lima', 'ana.lima@email.com', 'Aracaju', 'Sergipe'),
+         ('André', 'Silva', 'andre.silva@email.com', 'Aparecida de Goiânia', 'Goiás');
