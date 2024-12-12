048d778be9d9cbf99efb3cc15d904e7ac0b6fe94
OsmarAmaral <135924752+OsmarAmaral@users.noreply.github.com>
2024-12-12 15:10:12 -0400

    Create controle_de_usuarios.py
    esta a versão beta do software de inserir novos usuarios no banco de dados do Aquapulse
    
    dsc: feito em python

---
+++ controle_de_usuarios.py
@@ -0,0 +1,46 @@
+# controle de usuarios
+
+nomes = []
+senhas = []
+emails = []
+
+def criar_conta (nome, senha, email):
+    nomes.append(nome)
+    senhas.append(senha)
+    emails.append(email)
+
+def entrar_na_conta(email):
+    if email in email:
+        print("conta existente !")
+    else:
+        print("está conta não existe")
+    
+    
+# desenvolvimento
+# não mexer
+
+slc = 0
+while slc != 9:
+    print("-----Menu-----")
+    print("1. sing in")
+    print("2. sing up")
+    print("3. exit")
+    slc = int(input("select: "))
+    match slc:
+        case 1:
+            email = input("digite seu email: ")
+            print(entrar_na_conta(email))
+        case 2:
+            print("seja bem-vindo")
+            nome = input("digite seu nome: ")
+            senha = input("digite sua senha: ")
+            email = input("digite seu email: ")
+            print(criar_conta(nome, senha, email))
+        case 3:
+            print("sucesso!")
+        case 8:
+            print(nomes)
+            print(senhas)
+        case _:
+            print("undefined")
+        
