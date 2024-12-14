61863dce701c93f94d233473915efcb58f407c72
OsmarAmaral <135924752+OsmarAmaral@users.noreply.github.com>
2024-12-14 19:53:27 -0400

    Update aquaPulse2047.sql
    Remove updates

--- aquaPulse2047.sql
+++ aquaPulse2047.sql
@@ -54,8 +54,8 @@ CREATE TABLE tbMensagensSuporte (
 
 
 INSERT INTO tbAdministradores (nome_adm, sobrenome_adm, email_adm, cargo_adm)
-  VALUES ('Osmar', 'Araujo', 'osmar@gmail.com', 'chefe'),
-         ('Iago', 'Bortoluzzi', 'iago1@gmail.com', 'chefe'),
+  VALUES ('Osmar', 'Araujo', 'osmar.s@estudante.ifmt.edu.br', 'Tech Lead'),
+         ('Iago', 'Bortoluzzi', 'iagogabriel9710@gmail.com', 'chefe'),
          ('Pedro', 'Alves', 'pedro@gmail.com', 'estagiario'),
          ('Iago', 'Paiva', 'iago2@gmail.com', 'estagiario');
 
@@ -190,11 +190,3 @@ INSERT INTO tbUsuarios (nome_usuario, sobrenome_usuario, email_usuario, cidade_u
          ('Zé', 'Pereira', 'ze.pereira@email.com', 'Zé Pimenta', 'Bahia'),  
          ('Zoe', 'Rocha', 'zoe.rocha@email.com', 'Zé de Sá', 'Maranhão'),  
          ('Zuleide', 'Souza', 'zuleide.souza@email.com', 'Zero', 'São Paulo');  
-
-UPDATE tbAdministradores
-SET email_adm = 'osmar.s@estudante.ifmt.edu.br', cargo_adm = 'Tech Lead'
-WHERE nome_adm = 'Osmar' AND sobrenome_adm = 'Araujo';
-
-UPDATE tbAdministradores
-SET email_adm = 'iagogabriel9710@gmail.com'
-WHERE nome_adm = 'Iago' AND sobrenome_adm = 'Bortoluzzi';
