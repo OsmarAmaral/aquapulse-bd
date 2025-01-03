43c89c2678f7e6714286f0fa869b78eccb059982
OsmarAmaral <135924752+OsmarAmaral@users.noreply.github.com>
2025-01-03 08:48:45 -0400

    remove frelancers

--- pg_aquapulse.sql
+++ pg_aquapulse.sql
@@ -48,10 +48,8 @@ INSERT INTO tbAdministradores (nome_adm, sobrenome_adm, email_adm, cargo_adm)
   VALUES ('Osmar', 'Araujo', 'osmar.s@estudante.ifmt.edu.br', 'Tech Lead'),
          ('Iago', 'Bortoluzzi', 'iagogabriel9710@gmail.com', 'chefe'),
          ('Pedro', 'Alves', 'pedro@gmail.com', 'estagiario'),
-         ('Iago', 'Paiva', 'iago2@gmail.com', 'estagiario'),
-         ('Amir', 'Tamlieh', 'amir.t@estudante.ifmt.edu.br', 'freelancer'),
-         ('Leonardo', 'Leite', 'leornardo@email.com', 'freelancer');
-
+         ('Iago', 'Paiva', 'iago2@gmail.com', 'estagiario');
+         
 INSERT INTO tbUsuarios (nome_usuario, sobrenome_usuario, email_usuario, cidade_usuario, estado_usuario)
   VALUES ('Ana', 'Lima', 'ana.lima@email.com', 'Aracaju', 'Sergipe'),
          ('André', 'Silva', 'andre.silva@email.com', 'Aparecida de Goiânia', 'Goiás'),
